f89bcc74ddcb73578866117dd53a34d83e55d4b4
aisley.schmeisser <aisley.schmeisser@MU-C1-16.mullen.pvt.k12.co.us>
2024-09-30 13:57:18 -0600

    Tried to make board but it is erroring

--- Wordle/src/Board.java
+++ Wordle/src/Board.java
@@ -1,5 +1,30 @@
 
 public class Board
 	{
+		
+		static String [][] board = new String [6][5];
+		
+		// public static void prepareBoard()
+			    {
+			      for (int row = 0; row < board.length; row ++)
+			      {
+			        System.out.println();
+			       for (int col = 0; col < board[0].length; col ++) 
+			         {
+			         board[row][col] = " ";
+			         }
+			    }
+			    }
 
+			    //public static void displayBoard()
+			  {
+			    System.out.println(" WORDLE ");
+			    System.out.println("- - - - - - - -");
+			    System.out.println("| "+board[0][0]+" | "+board[0][1]+" | "+board[0][2]+" | "+board[0][3]+" | "+board[0][4]+" | ");
+			    System.out.println("- - - - - - - -");
+			    System.out.println(" |"+board[1][0]+" | "+board[1][1]+" | "+board[1][2]+" | "+board[1][3]+" | "+board[1][4]+" | ");
+			    System.out.println("- - - - - - - -");
+			    System.out.println("| "+board[2][0]+" | "+board[2][1]+" | "+board[2][2]+" | "+board[2][3]+" | "+board[2][4]+" | ");
+			    System.out.println("- - - - - - - -");
+			  }
 	}
