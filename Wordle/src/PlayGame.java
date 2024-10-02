16e22f3a6d3206fdb3ba515733a59f1dab6bc134
aisleyschmeisser <aisleyschmeisser@localhost>
2024-10-02 10:47:45 -0600

    Board is set up and starting to import words

--- Wordle/src/PlayGame.java
+++ Wordle/src/PlayGame.java
@@ -3,8 +3,9 @@ public class PlayGame
 	{
 
 		public static void main(String[] args)
-			{
-				System.out.println("Hello! Let's play Wordle!");
+			{	
+				Board.prepareBoard();
+				Board.displayBoard();
 
 			}
 
